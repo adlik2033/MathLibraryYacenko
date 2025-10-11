d6ba7e2db96d1eb9fb88e899e1ffd7c46b3fbb33
yatsenko.aa2201 <yatsenko.aa2201@C0M107-L1>
2025-10-11 17:07:23 +0500

    Добавьте файлы проекта.

---
+++ MathLibrary.Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using MathLibraryYacenko;
+using System;
+
+Console.WriteLine("Демонстрация работы MathLibrary.dll");
+
+double x = 10, y = 4;
+
+Console.Write($"Сложение чисел {x} + {y} = ");
+Console.WriteLine(Calculator.Add(x, y));
+
+Console.Write($"Вычитание второго числа из первого {y} - {x} = ");
+Console.WriteLine(Calculator.Substract(x, y));
+
+Console.Write($"Умножение чисел {x} * {y} = ");
+Console.WriteLine(Calculator.Multiply(x, y));
+
+Console.Write($"Деление чисел без ошибки {x} / {y} = ");
+Console.WriteLine(Calculator.Divide(x, y));
+
+double a = 2, b = 0;
+
+Console.Write($"Деление чисел без ошибки {a} / {b} = ");
+Console.WriteLine(Calculator.Divide(a, b));
+
+Console.WriteLine("Проверка чисел на простоту");
+
+int[] numbersToCheck = { 1, 2, 3, 4, 17, 25, 97, 997};
+
+for (int i = 0; i < numbersToCheck.Length; i++)
+{
+    int num = numbersToCheck[i];
+    bool isPrime = Calculator.IsPrime(num);
+    Console.WriteLine($"Число {num} является простым? -> {isPrime}");
+}
+
+int z = 6, power = 3;
+Console.Write($"Возведение {x} в {power} степень числа = ");
+Console.WriteLine(Calculator.Power(z, power));
+
+int q = 12;
+Console.Write($"Факториал {q} = ");
+Console.WriteLine(Calculator.Factorial(q));
+
+Console.Write($"Факториал {q} = ");
+Console.WriteLine(Calculator.Factorial(q));
---
+++ MathLibraryYacenko/Calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibraryYacenko
+{
+    public class Calculator
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Substract (double x, double y)
+        {
+            return y - x;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            return (x * y);
+        }
+
+        public static double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("Ошибка: " + new DivideByZeroException("На ноль делить нельзя"));
+            }
+
+            return x / y;
+        }
+
+        public static double Power(double x, double power)
+        {
+            return Math.Pow(x, power);
+        }
+        // 15х14х13х12х11х10х9х7х6х5х4х3х2х1
+        public static double Factorial(int n)
+        {
+            int sum = 1;
+            if(n > 0)
+            {
+                for(int i = 1; i <= n; i++)
+                {
+                    sum = sum * i; 
+                }
+            
+            }
+            return sum;
+        }
+
+        // 2x2 + 2xy + 3y2
+        public static void SolveQuadratic(double x, double y, double z, out double? x1, out double? x2)
+        {
+            double d = Math.Pow(y, 2) + (4*x*y);
+            x1 = (-y + Math.Sqrt(d))/2*x;
+            x2 = (-y - Math.Sqrt(d)) / 2 * x;
+        }
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number <= 3) return true;
+            if (number % 2 == 0 || number % 3 == 0) return false;
+            int j = (int)Math.Sqrt(number);
+
+            for (long i = 2; i <= j; i++) 
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
